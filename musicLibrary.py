67e76bbb6f14c53c285e4e902f14448452c1ef6f
sania51234 <saniaanjum51234@gmail.com>
2024-10-16 21:01:57 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "into it": "https://youtu.be/lZp96uELegI?si=5kktZl-TiaYV77o5",
+    "to you": "https://youtu.be/7q9Ubv3othc?si=zbPMZMavrAPsDppx",
+    "monster": "https://youtu.be/EbEJFWjiops?si=jEqcb_stH6_zetUx",
+    "confident": "https://youtu.be/qnD1hwjR3WU?si=Qmz0XhlnDd1uYzYM"
+}
